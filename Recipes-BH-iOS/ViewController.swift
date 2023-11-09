783c5f3554b0900f1bf5b3b8789e98d129e9cdbd
Gratianus Martin B <martin.gratianus@hotmail.com>
2023-11-09 16:08:01 +0700

    Initial Commit

---
+++ Recipes-BH-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Recipes-BH-iOS
+//
+//  Created by Martin Brianto on 09/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
